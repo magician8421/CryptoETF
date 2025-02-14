d3af0a28c6b0f2b9116d5b10945058e4a8df0b1a
xiaotong <83378122@163.com>
2025-02-14 08:31:21 +0800

    integrate uniswapv3 as oracle

---
+++ contracts/uniswap/UniswapV3TWAP.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.28;
+import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Factory.sol";
+import "uniswap-v3-periphery-0.8/contracts/libraries/OracleLibrary.sol";
+contract UniswapV3TWAP{
+    address public immutable token0;
+    address public immutable token1;
+    address public immutable pool;
+
+    constructor(address _factory,address _token0,address _token1,uint24 _fee){
+        token0=_token0;
+        token1=_token1;
+        address _pool=IUniswapV3Factory(_factory).getPool(_token0,_token1,_fee);
+        require(_pool!=address(0),"pool dosen't exist");
+        pool=_pool;
+        
+    }   
+    function estimateAmountOut(
+        address tokenIn,
+        uint128 amountIn,
+        uint32 secondsAgo
+    )  external view returns (uint amountOut) {
+        require(tokenIn==token0||tokenIn==token1,"Invalid token");
+        address tokenOut=tokenIn==token0?token1:token0;
+        (int24 tick,)  =OracleLibrary.consult(pool,secondsAgo);
+        amountOut=OracleLibrary.getQuoteAtTick(tick,amountIn,tokenIn,tokenOut);
+    }
+}
--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,11 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    hardhat: {
+      forking: {
+        url: "https://eth-mainnet.g.alchemy.com/v2/I3eHFhWUQaZueOZP5BPt3jdFLebK9aEe",
+      },
+    },
+  },
 };
